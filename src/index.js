54a1b0053bbc61e7580d258678754937e3560748
Daniel Salazar <DanielSalazr@hotmail.com>
2021-06-11 23:56:19 -0500

    14. Backup del proyecto antes de realizar el borrado de todos los archivos para empezar a tomar el curso desde cero

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
+//import './global.css';
+//import App from './components/App';
 
+//const container = document.getElementById('app');
+
+//ReactDOM.render(<App />, container);
+
+
+/*
+const element = React.createElement('a', 
+    {href:'https://google.com'},
+    'Ir a google');
+*/
+const name = 'el dani'
+const jsx =  <div>  
+    <h1> hello perritos badges spy {name}</h1>
+    <p> Soy el dani</p>
+</div>
+//const element = React.createElement('h1', {}, `Hola soy el ${name}`)
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'Hola soy el perris'),
+    React.createElement('p', {}, 'el amigo del ms perrisimo')
+);
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render( element , container);
