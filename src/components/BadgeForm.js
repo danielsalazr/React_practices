09f3e59dd32439c0f01d5849bc99dc0b40650659
Daniel Salazar <DanielSalazr@hotmail.com>
2021-06-13 13:41:54 -0500

    16.Levantamiento del estado de badgeFroms a badgeNews

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+
+    /**state = {
+        
+      };*/
     // evento
-    handleChange = (e) => {
-        //console.log({e});
-        console.log({name:e.target.name,
-            value:e.target.value});
-    }
+    // handleChange = (e) => {
+    //     //console.log({e});
+    //     /*console.log({name:e.target.name,
+    //         value:e.target.value});
+    //     */
+    //    this.setState({
+    //        [e.target.name]: e.target.value, //esto es importante para manejar el estado
+    //    })
+    // }
     // evento
     handleClick = () => {
         //console.log({e});
@@ -17,7 +25,8 @@ class BadgeForm extends React.Component {
         //console.log({e});
         //console.log("Hubo un submit");
         e.preventDefault(); // esto detiene el submit 
-        console.log("se evito el submit")
+        console.log(this.state) // Imprimimos las variables de estado
+        //console.log({this.state.firstName})
     }
 
 
@@ -29,8 +38,58 @@ class BadgeForm extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="">First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+                        <input onChange={this.props.onChange} 
+                        className="form-control" 
+                        type="text" 
+                        name="firstName" 
+                        value={this.props.formValues.firstName} />
                     </div>
+
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input
+                        onChange={this.props.onChange}
+                        className="form-control"
+                        type="text"
+                        name="lastName"
+                        value={this.props.formValues.lastName}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                        onChange={this.props.onChange}
+                        className="form-control"
+                        type="email"
+                        name="email"
+                        value={this.props.formValues.email}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input
+                        onChange={this.props.onChange}
+                        className="form-control"
+                        type="text"
+                        name="jobTitle"
+                        value={this.props.formValues.jobTitle}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input
+                        onChange={this.props.onChange}
+                        className="form-control"
+                        type="text"
+                        name="twitter"
+                        value={this.props.formValues.twitter}
+                        />
+                    </div>
+
+
                     <button onClick={this.handleClick} type="submit" className="btn btn-primary">save</button>
                 </form>
             </div>
--- src/components/pages/BadgeNew.js
+++ src/components/pages/BadgeNew.js
@@ -6,6 +6,35 @@ import Badge from '../Badge';
 import BadgeForm from '../BadgeForm';
 
 class BadgeNew extends React.Component {
+    state = {
+        // Para evotar los anuncios de warning es recomendable inivcializar los valores del formulario
+        form: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            jobTitle: '',
+            twitter: '',
+        }
+    };
+
+    handleChange = (e) => {
+
+        // las 2 siguientes lineas se ponen por  ue setState sobreescribe el estado de form
+        // con esto se evita que solo se guarde una variable y se puedan almacenar todas las 
+        // que deben almacenarse en form
+
+        // const nextForm = this.state.form
+        // nextForm[e.target.name] = e.target.value
+
+        this.setState({
+            //form : nextForm
+            form :{
+                ...this.state.form, // esta linea reemplaza las anteriores que se utilizan para evitar la sobreescritura
+            [e.target.name] : e.target.value
+            }
+            
+        })
+    }
     render() {
         return (
             <div>
@@ -16,14 +45,21 @@ class BadgeNew extends React.Component {
                 <div className="container">
                         <div className="row">
                             <div className="col">
-                                <Badge firstName="jerson" 
-                                lastName="Vasquez"
+                                <Badge 
+                                firstName={this.state.form.firstName} 
+                                lastName={this.state.form.lastName} 
                                 avatarUrl ="https://www.gravatar.com/avatar?d=identicon"
-                                jobTitle = "Listo para lo que venga"
-                                twitter = "danielsalaz"/>
+                                jobTitle = {this.state.form.jobTitle} 
+                                email ={this.state.form.email} 
+                                twitter = {this.state.form.twitter} 
+                                 />
                             </div>
                             <div className="col">
-                                <BadgeForm />
+                                <BadgeForm  
+                                onChange={this.handleChange} 
+                                formValues={this.state.form} 
+
+                                />
                             </div>
                         </div>
                     </div>
