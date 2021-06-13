f02706cbb15bb24400e1ddfab63ee07b0575433d
Daniel Salazar <DanielSalazr@hotmail.com>
2021-06-13 16:03:17 -0500

    incorporacion de estilo abadgeList y backup antes de integrar el react router

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,20 +1,45 @@
 import React from 'react';
+import './styles/BadgeList.css';
 
+
+class BadgesListItem extends React.Component {
+  render() {
+    return (
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.avatarUrl}
+          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+        />
+
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <br />@{this.props.badge.twitter}
+          <br />
+          {this.props.badge.jobTitle}
+        </div>
+      </div>
+    );
+  }
+}
 class BadgesList extends React.Component {
 
     render() {
         return(
-        <ul className="list-unlisted">
-                            {this.props.badges.map((badge) => {
-                                //ponemos el key para que react pueda verificar que los datos se renderisen sin repetirse
-                                return (
-                                    <li key={badge.id}>   
-                                        <p>
-                                            {badge.firstName} {badge.lastName}
-                                        </p>
-                                    </li>)
-                            })}
-                        </ul>
+            <div className="list-unlisted">
+                <ul className="list-unstyled" >
+                    {this.props.badges.map((badge) => {
+                        //ponemos el key para que react pueda verificar que los datos se renderisen sin repetirse
+                        return (
+                            <li key={badge.id}>   
+                                <BadgesListItem badge={badge} />
+                            </li>)
+                    })}
+                </ul>
+            </div>
+        
                         )
     }
 }
