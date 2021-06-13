503df8b86105c94b00493338d34523f3a9be506b
Daniel Salazar <DanielSalazr@hotmail.com>
2021-06-12 23:48:43 -0500

    15.Clase de inclusion de estilos CSS al proyecto y uso de JSX

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,36 +1,35 @@
 import React from 'react';
 
-import './styles/Badge.css';
+import "./styles/Badge.css"
 import confLogo from '../images/badge-header.svg';
 
+// Los componentes son clases 
+// La clase Badge extendera la clase React.component
 class Badge extends React.Component {
-  render() {
-    return (
-      <div className="Badge">
-        <div className="Badge__header">
-          <img src={confLogo} alt="Logo de la conferencia" />
-        </div>
 
-        <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="Avatar"
-          />
-          <h1>
-            {this.props.firstName} <br /> {this.props.lastName}
-          </h1>
-        </div>
+    // el metodo por defecto que requieren los componentes 
+    //  es render
 
-        <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.twitter}</div>
-        </div>
+    //En react utilizamos className en vez de class
+    render() {
+        return (<div className="Badge"> 
+            <h1>Badge</h1>
+            <div className="Badge__header">
+                <img src={confLogo} alt="Logo de la conferencia"/>
+            </div>
+            <div className='Badge__section-name'>
+                <img className='Badge__avatar' src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+                <h1>Soy el perris <br/> Salazar</h1>
+            </div>
+            <div className="Badge__section-info">
+                <h3>Ingeniero electronico</h3>
+                <div>@soyTuPapi</div>
+            </div>
+            <div className="Badge__footer"> #Ganador </div>
 
-        <div className="Badge__footer">#platziconf</div>
-      </div>
-    );
-  }
+        </div>)
+    }
 }
+
 
 export default Badge;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import './global.css'
+import Badge from './components/Badge';
+
 //import './global.css';
 //import App from './components/App';
 
@@ -15,7 +18,7 @@ const element = React.createElement('a',
     {href:'https://google.com'},
     'Ir a google');
 */
-const name = 'el dani'
+/*const name = 'el dani'
 const jsx =  <div>  
     <h1> hello perritos badges spy {name}</h1>
     <p> Soy el dani</p>
@@ -26,7 +29,8 @@ const element = React.createElement(
     {},
     React.createElement('h1', {}, 'Hola soy el perris'),
     React.createElement('p', {}, 'el amigo del ms perrisimo')
-);
+);*/
+
 const container = document.getElementById('app');
 
-ReactDOM.render( element , container);
+ReactDOM.render( <Badge /> , container);
